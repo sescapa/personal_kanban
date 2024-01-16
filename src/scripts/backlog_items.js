414f1acc24142b36ab15fb88443d7b9a55370654
Simon Chromebook <simon.escapa@gmail.com>
2024-01-16 19:23:13 +0100

    Changes to script order

--- src/scripts/backlog_items.js
+++ src/scripts/backlog_items.js
@@ -1,28 +1,28 @@
 function populate_kanban_board(){
 
-var backlog_list = document.getElementById("kanban_container");
+    //Placeholder
+    var number_of_milestones = 4
 
-//Placeholder
-var number_of_milestones = 4
+    //Interactive population
+    for(let num=1; num <= number_of_milestones; num++) {
+        fetch("data/backlog_items/item_" + num + ".json")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("HTTP error " + response.status);
+                }
+                return response.json();
+            })
+            .then(response => {
+                    var item_status = response['status']
+                    var item_name = response['name']
+                    var backlog_list = document.getElementById(item_status);
 
-//Placeholder
-for(let num=1; num <= number_of_milestones; num++) {
-    fetch("data/backlog_items/items_" + num + ".json")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("HTTP error " + response.status);
+                    var backlog_item = document.createElement('div')
+                    backlog_item.setAttribute("class", "item")
+                    backlog_item.innerHTML = item_name
+
+                    backlog_list.appendChild(backlog_item)
+                }
+                )
             }
-            return response.json();
-        })
-        .then(response => {
-            Object.keys(response).forEach(
-            key => {
-                var backlog_item = document.createElement('div')
-
-            }
-            )
-            }
-        )
-}
-
 }
