698b86e827960efd01c1aa277880d4dea21a20a0
Simon Chromebook <simon.escapa@gmail.com>
2024-01-18 01:27:46 +0100

    Adding button update functionality

--- src/scripts/milestones.js
+++ src/scripts/milestones.js
@@ -1,38 +1,41 @@
-function item_button(message, item_form_id, message){
+function item_button(message, milestone_id, item_form_div, message){
+
+    const item_form_ul = item_form_div + '_ul'
     function create_item(event){
     
-        document.getElementById("item_form_id").remove()
+        document.getElementById(item_form_ul).remove()
         event.preventDefault();
     };
     
     function update_item(event){
         
-        document.getElementById("item_form_id").remove()
+        document.getElementById(item_form_ul).remove()
         event.preventDefault();
     };
     
     function delete_item(event){
         
-        document.getElementById("item_form_id").remove()
+        document.getElementById(item_form_ul).remove()
         event.preventDefault();
     };
-    let item_form = document.getElementById(item_form_id)
+
+    let item_form = document.getElementById(item_form_div)
     let ul_input_list = []
 
     if (message === 'new'){
-        if(document.getElementById(item_form_id+"_ul")){
-            document.getElementById(item_form_id+"_ul").remove()
+        if(document.getElementById(item_form_ul)){
+            document.getElementById(item_form_ul).remove()
         } 
 
         let name_box = document.createElement('input');
         name_box.setAttribute("type","text")
         name_box.setAttribute("id", "backlog_form_name")
-        name_box.setAttribute("placeholder", "Milestone Name")
+        name_box.setAttribute("placeholder", "Item Name")
 
         let description_box = document.createElement('input');
         description_box.setAttribute("type","text")
         description_box.setAttribute("id", "backlog_form_description")
-        description_box.setAttribute("placeholder", "Milestone Description")
+        description_box.setAttribute("placeholder", "Item Description")
 
         let input_submit = document.createElement('input');
         input_submit.setAttribute("type","submit")
@@ -45,45 +48,59 @@ function item_button(message, item_form_id, message){
     }
     
     else if (message === 'update'){
-        if(document.getElementById(item_form+"_ul")){
-            document.getElementById(item_form+"_ul").remove()
+        if(document.getElementById(item_form_ul)){
+            document.getElementById(item_form_ul).remove()
         } 
-        let name_box = document.createElement('input');
-        name_box.setAttribute("type","text")
-        name_box.setAttribute("id", "backlog_form_name")
-        name_box.setAttribute("placeholder", "Milestone Name")
+
+        let milestone_input_box = document.createElement('input');
+        milestone_input_box.setAttribute("name", milestone_id+"_list");
+        milestone_input_box.setAttribute("list", milestone_id+"_update_datalist")
+
+        let milestone_datalist_box = document.createElement('datalist');
+        milestone_datalist_box.setAttribute("id", milestone_id+"_update_datalist");
+        
+        for(backlog_item of localData['backlog_items']){
+            if(backlog_item['milestone'] === milestone_id){
+                let milestone_option = document.createElement('option')
+                milestone_option.setAttribute("value", backlog_item['name'])
+                milestone_datalist_box.appendChild(milestone_option)
+            }
+        }
 
         let description_box = document.createElement('input');
         description_box.setAttribute("type","text")
-        description_box.setAttribute("id", "backlog_form_description")
-        description_box.setAttribute("placeholder", "Milestone Description")
+        description_box.setAttribute("id", milestone_id+ "_backlog_form_description")
+        description_box.setAttribute("placeholder", "Item Description")
 
         let input_submit = document.createElement('input');
         input_submit.setAttribute("type","submit")
         input_submit.setAttribute("value","Update")
         input_submit.addEventListener("click", update_item);
         
-        ul_input_list.push(name_box)
+        ul_input_list.push(milestone_input_box)
+        ul_input_list.push(milestone_datalist_box)
         ul_input_list.push(description_box)
         ul_input_list.push(input_submit)
     }
 
     else if (message === 'delete'){
-        if(document.getElementById(item_form_id+"_ul")){
-            document.getElementById(item_form_id+"_ul").remove()
+        if(document.getElementById(item_form_ul)){
+            document.getElementById(item_form_ul).remove()
         } 
 
         let milestone_input_box = document.createElement('input');
         milestone_input_box.setAttribute("name", "milestone_list");
-        milestone_input_box.setAttribute("list", "milestone_delete_datalist")
+        milestone_input_box.setAttribute("list", milestone_id+"_delete_datalist")
 
         let milestone_datalist_box = document.createElement('datalist');
-        milestone_datalist_box.setAttribute("id", "milestone_delete_datalist");
+        milestone_datalist_box.setAttribute("id", milestone_id+"_delete_datalist");
         
-        for(milestone of localData['milestones']){
-            let milestone_option = document.createElement('option')
-            milestone_option.setAttribute("value", milestone['name'])
-            milestone_datalist_box.appendChild(milestone_option)
+        for(backlog_item of localData['backlog_items']){
+            if(backlog_item['milestone'] === milestone_id){
+                let milestone_option = document.createElement('option')
+                milestone_option.setAttribute("value", backlog_item['name'])
+                milestone_datalist_box.appendChild(milestone_option)
+            }
         }
 
         let input_submit = document.createElement('input');
@@ -97,7 +114,7 @@ function item_button(message, item_form_id, message){
     }
 
     let form_ul = document.createElement('ul')
-    form_ul.setAttribute('id', 'item_form_ul')
+    form_ul.setAttribute('id', item_form_ul)
     for(let box of ul_input_list){
         form_ul.appendChild(box);
     }
@@ -242,7 +259,7 @@ function milestone_dropdown(){
         item_button_new.setAttribute("class", "item_button")
         item_button_new.setAttribute("id", milestone_id + "_button_new")
         item_button_new.addEventListener("click", function(e) {
-            item_button(e, milestone_id + '_item_form', 'create');
+            item_button(e, milestone_id, milestone_id + '_div_item_form_div', 'new');
         });
 
         const item_button_update = document.createElement('button')
@@ -250,33 +267,44 @@ function milestone_dropdown(){
         item_button_update.setAttribute("class", "item_button")
         item_button_update.setAttribute("id", milestone_id + "_button_update")
         item_button_update.addEventListener("click", function(e) {
-            item_button(e, milestone_id + '_item_form', 'update');
+            item_button(e, milestone_id, milestone_id + '_div_item_form_div', 'update');
         });
         const item_button_delete = document.createElement('button')
         item_button_delete.innerHTML = "Delete Item"
         item_button_delete.setAttribute("class", "item_button")
         item_button_delete.setAttribute("id", milestone_id + "_button_delete")
         item_button_delete.addEventListener("click", function(e) {
-            item_button(e, milestone_id + '_item_form', 'delete');
+            item_button(e, milestone_id, milestone_id + '_div_item_form_div', 'delete');
         });
 
-        const item_form_div = document.createElement('div')
-        item_form_div.setAttribute("class", "milestone_button_div")
-        item_form_div.setAttribute("id", milestone_id + "_button_delete")
 
         milestone_buttons_div.appendChild(item_button_new)
         milestone_buttons_div.appendChild(item_button_update)
         milestone_buttons_div.appendChild(item_button_delete)
 
+        // Form for Create, Update and Delete
+
+        const item_form_div = document.createElement('div')
+        item_form_div.setAttribute("class", "milestone_button_div")
+        item_form_div.setAttribute("id", milestone_id + "_div_item_form_div")
+
+        const form_item_list = document.createElement('ul')
+        form_item_list.setAttribute("id", milestone_id + '_div_item_form_div_ul')
+        item_form_div.appendChild(form_item_list)
+
         // Milestone item list
-        const milestone_item_list = document.createElement('ul')
-        milestone_item_list.setAttribute("id", milestone_id + '_item_form')
+        const milestone_item_form_div = document.createElement('div')
+        milestone_item_form_div.setAttribute('id', milestone_id + '_div_milestone_item_form')
+
+        const item_list_ul = document.createElement('ul')
+        item_list_ul.setAttribute("id", milestone_id + '_div_milestone_item_form_ul')
+        milestone_item_form_div.appendChild(item_list_ul)
 
         // Append everything together
         milestone_details.appendChild(milestone_summary)
         milestone_details.appendChild(milestone_buttons_div)
         milestone_details.appendChild(item_form_div)
-        milestone_details.appendChild(milestone_item_list)
+        milestone_details.appendChild(milestone_item_form_div)
 
         milestone_div.appendChild(milestone_details)
         backlog_list.appendChild(milestone_div)
