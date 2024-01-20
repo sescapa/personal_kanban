e564d5b366d5271244f51097fec6a3a3f80589e1
Simon Chromebook <simon.escapa@gmail.com>
2024-01-20 17:30:15 +0100

    Adding unique ID as miliseconds

--- src/main.js
+++ src/main.js
@@ -1,68 +1,43 @@
 //Placeholder for the local storage
 
-const sample_project = {
-    "name": "Project Name",
-    "summary": "Summary",
-    "due_date": "Due Date",
-    "id": "id"
-}
 
-const sample_item = {
-    "name": "Task Name",
-    "due_date": "Due Date",
-    "milestone_id": "milestone_id",
-    "acceptance_criteria": "Acceptance Criteria",
-    "status": "status",
-    "id": "id"
-}
+function populateStorage() {
 
-const sample_milestone = {
-    "name": "Milestone Name",
-    "description": "Milestone Description",
-    "milestone_id": "id"
+    const project_id = 'id' + (new Date()).getTime()
+    const milestone_id = 'id' + (new Date()).getTime()
+    const item_id = 'id' + (new Date()).getTime()
+
+    const sample_project = {
+        "name": "Project Name",
+        "summary": "Summary",
+        "due_date": "Due Date",
+        "project_id": project_id
+    }
+
+    const sample_milestone = {
+        "name": "Milestone Name",
+        "description": "Milestone Description",
+        "milestone_id": milestone_id
+    }
+
+    const sample_item = {
+        "name": "Task Name",
+        "due_date": "Due Date",
+        "milestone_id": milestone_id,
+        "acceptance_criteria": "Acceptance Criteria",
+        "status": "status",
+        "id": item_id
+    }
+
+    localStorage.setItem("project", JSON.stringify(sample_project));
+    localStorage.setItem("milestones", JSON.stringify([sample_milestone]) );
+    localStorage.setItem("backlog_items", JSON.stringify([sample_item]) );
 }
 
 if (!localStorage.getItem("project")) {
     localStorage.clear()
     populateStorage();
-  } else {
-    setStyles();
   }
-  
-  function populateStorage() {
-    localStorage.setItem("project", JSON.stringify(testObject));
-    localStorage.setItem("font", );
-    localStorage.setItem("image", );
-  }
-
-let localData = {
-    "project":
-        ,
-    "milestones":,
-    "backlog_items":,
-    "template": {
-        "backlog_item_template": {
-            "name": "Task Name",
-            "due_date": "Due Date",
-            "milestone": "milestone",
-            "acceptance_criteria": "Acceptance Criteria",
-            "status": "status",
-            "id": "id"
-        },
-        "project_template": {
-            "name": "Project Name",
-            "summary": "Summary",
-            "due_date": "Due Date",
-            "id": "id"
-        },
-        "milestone_template":{
-            "name": "Milestone Name",
-            "description": "Milestone Description",
-            "id": "id"
-        }
-    }
-}
-
 
 project_page_header()
 milestone_dropdown()
